4b43565e3a9cbdd9328ef2c0e12883be2baffb50
shanehughes1990 <shane_hughes@live.ca>
2020-03-15 20:27:55 -0400

    removing uneeded files

--- config/omv/enable_iframe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-. /etc/default/openmediavault
-. /usr/share/openmediavault/scripts/helper-functions
-omv_set_default OMV_NGINX_SITE_WEBGUI_SECURITY_CSP_ENABLE 0
-omv_set_default OMV_NGINX_SITE_WEBGUI_SECURITY_XFRAMEOPTIONS_ENABLE false
-omv-mkconf nginx
-invoke-rc.d nginx restart
-omv_purge_internal_cache
