5e32e959a666ca5f3e4941f348cbc55f1e1960e2
shanehughes1990 <shane_hughes@live.ca>
2020-03-29 19:48:34 -0400

    adding gcrypt_upload scripts

---
+++ config/prereq/gcrypt_upload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# RClone Config file
+RCLONE_CONFIG=/data/rclone/rclone.conf
+export RCLONE_CONFIG
+
+#exit if running
+if [[ "`pidof -x $(basename $0) -o %PPID`" ]]; then exit; fi
+
+# Move older local files to the cloud
+/usr/bin/rclone move /data/local_media/ gcrypt: \
+--log-file /data/rclone/logs/upload.log \
+-v \
+--exclude-from /data/rclone/scripts/excludes \
+--delete-empty-src-dirs \
+--user-agent 478201604 \
+--fast-list \
+--max-transfer 700G
